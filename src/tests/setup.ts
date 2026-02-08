3b2b4652bb4d235d8cb4a3870374df0e98bd8ff3
orca <dquanbao@gmail.com>
2026-02-08 21:52:33 +0700

    fix: register i18n to testing

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,5 +1,17 @@
 import { vi } from "vitest";
 import { config } from "@vue/test-utils";
+import { createI18n } from "vue-i18n";
+import { EnglishLocale } from "../i18n/locales/en";
+import { VietnameseLocale } from "../i18n/locales/vi";
+
+const i18n = createI18n({
+  legacy: false,
+  locale: "en",
+  messages: {
+    en: EnglishLocale,
+    vi: VietnameseLocale
+  },
+});
 
 // Mock Chrome API (types from @types/chrome)
 (globalThis as any).chrome = {
@@ -45,3 +57,5 @@ import { config } from "@vue/test-utils";
 config.global.stubs = {
   teleport: true,
 };
+
+config.global.plugins = [i18n];
